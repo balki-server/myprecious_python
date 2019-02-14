416119b5b2af4a2417d1039af012685480eb0470
Balki <balki.kodarapu@gmail.com>
2019-02-13 17:56:57 -0800

    Update main script to use pypi API instead of scraping the website directly

--- lib/myprecious_python.rb
+++ lib/myprecious_python.rb
@@ -1,7 +1,8 @@
-require 'nokogiri'
-require 'open-uri'
 require 'date'
 require 'byebug'
+require 'httparty'
+require 'json'
+require 'rest-client'
 
 class MyPreciousPython
   def self.update
@@ -14,6 +15,7 @@ class MyPreciousPython
     gem_change_pos = 5
     default_length = 6
     if File.file?('dependency-tracking.md')
+      puts "reading the dependency file...."
       File.open("dependency-tracking.md", "r").each_with_index do |line, line_number|
         #puts line + " dds " + line_number.to_s
         word_array = line.split('|')
@@ -45,7 +47,7 @@ class MyPreciousPython
       end
       #puts gem_lines
     else
-      puts "creating the file"
+      puts "creating the dependency file...."
       File.open('dependency-tracking.md', 'w') { |write_file|
         write_file.puts "Dependency | Our Version | Latest Version | Date available | Age (in days) | Change Log"
         write_file.puts "--- | --- | --- | --- | --- | ---"
@@ -58,89 +60,97 @@ class MyPreciousPython
 
     while (line = file.gets)
       gem_line = line.strip
+      
       if !gem_line.start_with?('#') && !gem_line.start_with?("https://")
-        
-        if gem_line.include? "=="
-          name = gem_line.split('==')[0]
-          current_version = gem_line.split('==')[1] 
-        elsif gem_line.include? ">="
-          name = gem_line.split('>=')[0]
-          current_version = gem_line.split('>=')[1] 
-        elsif gem_line.include? "<="
-          name = gem_line.split('<=')[0]
-          current_version = gem_line.split('<=')[1] 
-        end
-        if !current_version.index(';').nil?
-          current_version = current_version.split(';')[0]
-        end  
-        latest_version_string = %x(yolk -U #{name})
         begin
-          latest_version = latest_version_string.split(' ')[2].split("(")[1].split(")")[0]
-        rescue
-          latest_version = current_version
-        end
-        scrape_url = "https://pypi.python.org/pypi/" + name.to_s + "/" + current_version.to_s
-        begin
-          doc = Nokogiri::HTML(open(scrape_url))
-          doc_data = doc.css('tr.odd td')
-          if !doc_data.nil?
-            current_build_date = doc_data[3].content
-          else
+          if gem_line.include? "=="
+            name = gem_line.split('==')[0]
+            current_version = gem_line.split('==')[1] 
+          elsif gem_line.include? ">="
+            name = gem_line.split('>=')[0]
+            current_version = gem_line.split('>=')[1] 
+          elsif gem_line.include? "<="
+            name = gem_line.split('<=')[0]
+            current_version = gem_line.split('<=')[1] 
+          end
+          if !current_version.index(';').nil?
+            current_version = current_version.split(';')[0]
+          end  
+
+          api_url = "https://pypi.python.org/pypi/" + name + "/" + "json"
+          response = RestClient.get(api_url)
+          puts name.to_s + ": gathering info @ " + api_url
+          begin
+            current_build_date = JSON.parse(response)['releases'][current_version][0]['upload_time']
+            if !current_build_date.nil?
+              current_build_date = current_build_date
+            else
+              current_build_date = ""
+            end
+          rescue Exception => e
+            puts e
             current_build_date = ""
           end
-        rescue Exception => e
-          current_build_date = ""
-        end
-        puts name.to_s + " gathering info @" + scrape_url = "https://pypi.python.org/pypi/" + name.to_s + "/" + latest_version
-        puts scrape_url
-        doc = Nokogiri::HTML(open(scrape_url))
-        begin
-          doc_data = doc.css('tr.odd td')
-          if !doc_data.nil?
-            latest_build_date = doc_data[3].content
-          else
+          
+          begin
+            latest_version = JSON.parse(response)['info']['version']
+          rescue
+            puts e
+            latest_version = ""
+          end          
+                    
+          begin
+            latest_build_date = JSON.parse(response)['releases'][latest_version][0]['upload_time']
+            if !latest_build_date.nil?
+              latest_build_date = latest_build_date
+            else
+              latest_build_date = ""
+            end
+          rescue Exception => e
+            puts e
             latest_build_date = ""
           end
+
+          begin
+            documentation = JSON.parse(response)['info']['project_url']
+          rescue Exception => e
+            puts e
+            documentation = ""
+          end
+
+          begin
+            homepage_uri = JSON.parse(response)['info']['home_page']
+          rescue Exception => e
+            puts e
+            homepage_uri = ""
+          end
+
+          begin
+            days_complete = Date.parse(latest_build_date) - Date.parse(current_build_date)
+          rescue Exception => e
+            days_complete = 0
+          end 
+           
+          if gem_lines[name].nil?
+            array_to_write = Array.new(default_length) { |i| "" }
+          else
+            array_to_write = final_write[gem_lines[name]].split('|')
+          end
+          array_to_write[gem_name_pos] = "[" + name + "]" + "(" + homepage_uri.to_s  + ")"
+          array_to_write[gem_version_pos] = current_version.to_s
+          array_to_write[gem_latest_pos] = latest_version.to_s
+          array_to_write[gem_date_pos] = (latest_build_date).to_s
+          array_to_write[gem_age_pos] = days_complete.to_i.to_s
+
+          array_to_write[gem_change_pos] = documentation.to_s.to_s + "\n"
+          if !gem_lines[name].nil?
+            final_write[gem_lines[name]] = array_to_write.join("|")
+          else
+            final_write << array_to_write.join("|")
+          end
         rescue Exception => e
-          latest_build_date = ""
+          puts e
         end
-
-        begin
-          documentation = doc.at('li:contains("Documentation:")').css('a').map { |link| link['href'] }[0]
-        rescue Exception => e
-          documentation = ""
-        end
-
-        begin
-          homepage_uri = doc.at('li:contains("Home Page")').text.strip.split("\n").last.strip
-        rescue Exception => e
-          homepage_uri = ""
-        end
-
-        begin
-          days_complete = Date.parse(latest_build_date) - Date.parse(current_build_date)
-        rescue Exception => e
-          days_complete = 0
-        end 
-         
-        if gem_lines[name].nil?
-          array_to_write = Array.new(default_length) { |i| "" }
-        else
-          array_to_write = final_write[gem_lines[name]].split('|')
-        end
-        array_to_write[gem_name_pos] = "[" + name + "]" + "(" + homepage_uri.to_s  + ")"
-        array_to_write[gem_version_pos] = current_version.to_s
-        array_to_write[gem_latest_pos] = latest_version.to_s
-        array_to_write[gem_date_pos] = (latest_build_date).to_s
-        array_to_write[gem_age_pos] = days_complete.to_i.to_s
-
-        array_to_write[gem_change_pos] = documentation.to_s.to_s + "\n"
-        if !gem_lines[name].nil?
-          final_write[gem_lines[name]] = array_to_write.join("|")
-        else
-          final_write << array_to_write.join("|")
-        end
-
       end
     end
     File.open('dependency-tracking.md', 'w') { |f| f.write(final_write.join) }
--- myprecious_python.gemspec
+++ myprecious_python.gemspec
@@ -1,15 +1,18 @@
 Gem::Specification.new do |s|
   s.name        = 'myprecious_python'
-  s.version     = '0.0.3'
-  s.date        = '2017-04-26'
+  s.version     = '0.0.6'
+  s.date        = '2019-02-13'
   s.summary     = "Your precious python dependencies!"
   s.description = "A simple, markdown generated with information about your python dependencies"
   s.authors     = ["Balki Kodarapu"]
   s.email       = 'balki.kodarapu@gmail.com'
   s.files       = ["lib/myprecious_python.rb"]
   s.executables << 'myprecious_python'
-  s.add_runtime_dependency 'nokogiri', '~> 1.7', '>= 1.7.0'
   s.add_runtime_dependency 'byebug', '~> 9.0', '>= 9.0.6'
+  s.add_runtime_dependency 'httparty', '~> 0.16', '>= 0.16.4'
+  s.add_runtime_dependency 'json', '~> 2.1.0', '>= 2.1'
+  s.add_runtime_dependency 'rest-client', '~> 2.0.2', '>= 2.0'  
+  
   s.homepage    =
     'http://rubygems.org/gems/myprecious_python'
   s.license       = 'MIT'
